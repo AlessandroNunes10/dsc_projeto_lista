61bfa98e94f36d0cd12840c2aea710d8155e1f2c
AlessandroNunes10 <alessandro.nfx10@gmail.com>
2023-11-01 20:18:48 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/edu/ifrn/projetolista/ProjetoListaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.projetolista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoListaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoListaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifrn/projetolista/ProjetoListaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrn.projetolista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoListaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
